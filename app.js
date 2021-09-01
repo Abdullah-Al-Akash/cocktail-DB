7d9fa01d9f3cc15ee05d987863e4e352d11ef4b9
Abdullah Al Akash <aabdullahalakash@gmail.com>
2021-09-01 20:29:50 +0600

    Added Single Drink By ID

--- app.js
+++ app.js
@@ -17,32 +17,37 @@ document.getElementById('search-btn').addEventListener('click', function () {
         }
 
         // Fetch URL:
+        else if (searchText.length == 1) {
+                // Searchwith single word:
+                fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${searchText}`)
+                        .then(res => res.json())
+                        .then(data => showDrinks(data.drinks));
+        }
         else {
+                // Load Drinks Item
                 fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchText}`)
                         .then(res => res.json())
-                        .then(data => showDrinks(data.drinks))
+                        .then(data => showDrinks(data.drinks));
         }
-
         searchField.value = '';
 })
 
-
+// Show Drinks Item:
 function showDrinks(drinks) {
-
+        // console.log(drinks)
         if (!drinks) {
                 const div = document.createElement('div');
                 div.innerHTML = `
-                        <h4 class="text-danger text-center">There is no drinks...</h4>
+                        <h4 class="text-danger text-center">There are no drinks...</h4>
                 `;
                 errorMsg.appendChild(div);
         }
 
         else {
                 drinks?.forEach(drink => {
-                        console.log(drink)
                         const div = document.createElement('div');
                         div.innerHTML = `
-                                <div class="col">
+                                <div onclick=singleDrink('${drink.idDrink}') class="col">
                                         <div class="card drinks">
                                                 <img src="${drink.strDrinkThumb}" class="img-fluid p-4 rounded-3" alt="...">
                                                 <div class="card-body">
@@ -56,4 +61,15 @@ function showDrinks(drinks) {
                 })
         }
 
+}
+// Load Single Drink By ID:
+const singleDrink = (drink) => {
+        fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drink}`)
+                .then(res => res.json())
+                .then(data => showSingleDrink(data.drinks[0]))
+}
+
+// Show Single Drink Details:
+const showSingleDrink = (drink) => {
+        console.log(drink);
 }
